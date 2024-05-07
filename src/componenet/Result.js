93040439474278841c725e7c2c5d1269e82151cf
nixon100 <nixon6637@gmail.com>
2024-05-07 15:42:13 +0530

    task completed

--- src/componenet/Result.js
+++ src/componenet/Result.js
@@ -1,28 +1,78 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-const Result = () => {
-  const tasksList = useSelector((state) => state.Arrive?.tasksList) || []
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import {
+  addTotalToList,
+  addQty,
+  Minus,
+  Plus,
+  Remove,
+} from "../slices/tasksSlice";
+import Item from "./Item";
 
+const Result = () => {
+  const { tasksList, total } = useSelector((state) => state.tasks);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const prices = tasksList.map((product) => product.price);
+    const totall = prices.reduce((acc, price) => acc + price, 0);
+    dispatch(addTotalToList(totall));
+  }, [tasksList]);
+
+  useEffect(() => {
+    dispatch(addQty());
+    console.log("sdsdsds");
+  }, []);
+
+  const Minuss = (index) => {
+    dispatch(Minus(index));
+  };
+
+  const Pluss = (id) => {
+    dispatch(Plus(id));
+  };
+
+  const Removee = (id) => {
+    dispatch(Remove(id));
+  };
   return (
     <div className="CartContainer">
-    <div className="Header">
-      <h3 className="Heading">Shopping Cart</h3>
-      <h5 className="Action">Remove all</h5>
-    </div>
-
-    <hr />
-    <div className="checkout">
-      <div className="total">
-        <div>
-          <div className="Subtotal">Sub-Total</div>
-          <div className="items">{tasksList.length > 1 ? `${tasksList.length} items` : `${tasksList.length} item`}</div>
-        </div>
-        <div className="total-amount"></div>
+      <div className="Header">
+        <h3 className="Heading">Shopping Cart</h3>
+        <h5 className="Action">Remove all</h5>
       </div>
-      <button className="button">Checkout</button>
-    </div>
-  </div>
-  )
-}
 
-export default Result
+      {tasksList.map((item, index) => (
+        <>
+          {/* Call the Calculate function with the item as an argument */}
+          <Item
+            key={index}
+            item={item}
+            pri={() => Pluss(index)}
+            pri1={() => Minuss(index)}
+            onRemove={()=>Removee(index)}
+          />
+        </>
+      ))}
+
+      <hr />
+      <div className="checkout">
+        <div className="total">
+          <div>
+            <div className="Subtotal">Sub-Total</div>
+            <div className="items">
+              {tasksList.length > 1
+                ? `${tasksList.length} items`
+                : `${tasksList.length} item`}
+            </div>
+          </div>
+          <div className="total-amount">{total}</div>
+        </div>
+        <button className="button">Checkout</button>
+      </div>
+    </div>
+  );
+};
+
+export default Result;
--- src/slices/tasksSlice.js
+++ src/slices/tasksSlice.js
@@ -1,15 +1,46 @@
 import { createSlice } from "@reduxjs/toolkit";
-import Pro from '../componenet/Products.json'
-const initialState ={
-    tasksList : [Pro.products]
-}
-const TasksSlice = createSlice({
-    name : "tasksSlice",
-    initialState,
-    reducers:{
-
-    }
-
-})
-
-export default TasksSlice.reducer
+import productsData from "../componenet/Products.json";
+const initialState = {
+  tasksList: productsData.products,
+  total: 0,
+};
+const tasksSlice = createSlice({
+  name: "tasksSlice",
+  initialState,
+  reducers: {
+    addTotalToList: (state, action) => {
+      state.total = action.payload;
+      //   console.log(action.payload);
+    },
+    addQty: (state, action) => {
+      const a = state.tasksList.map((e) => ({ ...e, quantity: 1 }));
+      state.tasksList = a;
+      //   console.log(state.tasksList)
+    },
+    Minus: (state, action) => {
+      const newItems = [...state.tasksList];
+      if (newItems[action.payload].quantity > 1) {
+        newItems[action.payload].quantity -= 1;
+        newItems[action.payload].price =
+          newItems[action.payload].quantity *
+          productsData.products[action.payload].price;
+      }
+      state.tasksList = newItems;
+    },
+    Plus: (state, action) => {
+      console.log(action.payload);
+      const newItems = [...state.tasksList];
+      newItems[action.payload].quantity += 1;
+      newItems[action.payload].price =
+        newItems[action.payload].quantity *
+        productsData.products[action.payload].price;
+      state.tasksList = newItems;
+    },
+    Remove: (state, action) => {
+      const newItems = state.tasksList.filter((_, i) => i !== action.payload);
+      state.tasksList = newItems;
+    },
+  },
+});
+export const { addTotalToList, addQty, Minus, Plus,Remove } = tasksSlice.actions;
+export default tasksSlice.reducer;
